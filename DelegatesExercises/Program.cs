95d308984d9cb14a36464894d00023a5a44d84c6
juliocelon <grolain@hotmail.com>
2022-03-01 11:31:50 -0600

    first commit

---
+++ DelegatesExercises/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace DelegatesExercises
+{
+    public delegate String myMethodDelegate(int myInt);
+
+    public class Class1
+    {
+        public String method1(int value)
+        {
+            return string.Format("Method 1, value=[{0}]", value);
+        }
+
+        public static String method2(int value)
+        {
+            return string.Format("Method 2, value=[{0}]", value);
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Class1 c1 = new Class1();
+
+            myMethodDelegate method1 = new myMethodDelegate(c1.method1);
+            myMethodDelegate method2 = new myMethodDelegate(Class1.method2);
+
+            Console.WriteLine("{0}", method1(1));
+            Console.WriteLine("{0}", method2(2));
+        }
+    }
+}
